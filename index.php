97dcc057093658f90f0d2985cb2549d531331813
Kamil Machanski <kamilmachanski@gmail.com>
2019-04-05 22:53:34 +0200

    begin work

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+$tablica = [1,2,3,4,5];
+
+echo $tablica[0].'<br/>';
+
+$n = count($tablica);
+for ($i=0; $i < $n ; $i++) {
+  // code...
+  echo $tablica[$i].'<br/>';
+}
+*/
+include('utils/utils.php');
+
+$tablica = [1,2,3,4,5];
+
+dump($tablica);
+
+ ?>
