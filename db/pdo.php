1b02ab409e17fd7fa48ba5195d957eaa58781140
Kamil Machanski <kamilmachanski@gmail.com>
2019-04-26 16:28:01 +0200

    polish signs

--- db/pdo.php
+++ db/pdo.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO("oci:dbname=//localhost:1521/test", 'kam', '1234');
+    $pdo = new PDO("oci:dbname=//localhost:1521/test;charset=UTF8", 'kam', '1234');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // zwraca tablice assoc
 
